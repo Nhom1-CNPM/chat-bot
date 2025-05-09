f5a8ebf3ec1d160c3b3c9e968618e84b014eb3f6
Yoshi IT <136799047+loctran312@users.noreply.github.com>
2025-05-09 16:55:58 +0700

    Add files via upload

---
+++ chat-bot/js/chatbot.js
@@ -0,0 +1,59 @@
+const Gemini_Key = "AIzaSyC4k_eIKojbKs5ccVMMzMZpdnXA_BWqEJ4"
+document.querySelector(".inputarea button").addEventListener("click", sendMessage);
+
+const conversations = [
+    
+]
+
+function sendMessage(){
+    const userMessage = document.querySelector(".inputarea input").value;
+    if(userMessage.length){
+        document.querySelector(".inputarea input").value="";
+        document.querySelector(".chatwindow .chat").insertAdjacentHTML("beforeend",`<div class="user">
+                <p>${userMessage}</p>
+            </div>`)
+    }
+
+    conversations.push(
+        {
+        "role": "user",
+        "parts": [
+          {
+            "text": userMessage
+          }
+        ]
+        }
+    );
+
+    goiDuLieu(conversations);
+}
+
+async function goiDuLieu(conversations) {
+    console.log(conversations);
+    const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="+Gemini_Key, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                contents: conversations,
+        }),
+    });
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector(".chatwindow .chat").insertAdjacentHTML("beforeend",`<div class="model">
+                <p>${data.candidates[0].content.parts[0].text}</p>
+            </div>`)
+
+    conversations.push(
+        {
+        "role": "model",
+        "parts": [
+        {
+            "text": data.candidates[0].content.parts[0].text,
+        }
+        ]
+    },
+    )
+}
